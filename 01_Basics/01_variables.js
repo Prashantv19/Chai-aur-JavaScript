5baa583fb0cf5a8fcb676b6ca73eb9cdf4e02caa
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-21 22:23:02 +0530

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 2323    // ye only constant define karne ke liye use hota hai.
+let accountEmail = "Prashant@gmail.com"  // isme scope problem nahi hai this is only differance between 'let' and 'var'
+var accountPassword = "1234"   // 'var' have scope problem due to same variable name
+accountCity = "Jaunpur"   // we can define it without variable keyword but not recommended
+
+let accountState; // it will show undefinede because uski value nahi likhi hai.
+
+// accountId = 2  // it can not be changed
+accountEmail = "Prasha@gmail.com"
+accountPassword = "98456984"
+accountCity = "vns"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use 'var'
+because of block scope , functional scope problem
+*/
+
