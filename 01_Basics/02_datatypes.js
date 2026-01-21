b60ab667391b22beff23a7ea65eae6e3e68ab80e
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-21 23:57:28 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+//alert (3+4) //we are usimnh node js not browser
+ 
+console.log("prashant"); console.log("Vishwakarma");  //agar ek hi line me 2 print staement likhane hai toh ';' use karna padega. but this is not recommended ever.
+//write readable code always.
+
+
+
+let name = "Prashant"
+let age = "23"
+let isLoggedIn = false
+
+let state;  // => undefined
+
+//number => 2 to power 53
+//biginit
+//string => ""
+//boolean => true/false
+// null => standalone value  //intentionly not defined
+// undefined => //variable decleared but value not defined 
+// symbol => unique
+//object => 
+
+    console.log(typeof"Prasahnt" );
+    console.log(typeof null);   // type of null is object
+    console.log(typeof undefined);  //and type of undefinmed is undefined
+
+    const fruits = ["Banana", "Orange", "Apple"];
+console.log(typeof fruits);         // Output: "object"
+console.log(Array.isArray(fruits)); // Output: true
+
+    
+    
+    
+
