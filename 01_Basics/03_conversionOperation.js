3ae57ff115f5a494715b541a416d5b85fb4213bd
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-22 21:37:50 +0530

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "55"
+
+console.log(typeof score);  // it will show String datatype
+console.log(typeof (score)); // it will show String datatype
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+
