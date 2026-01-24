4c888ed78398b5f52c5f776815c24fcde778a541
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-24 23:25:30 +0530

    stringtill11min

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,19 @@
+const name= "Prashant"
+
+
+const repoCount = 22
+console.log(name + repoCount + "total");
+
+//string interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// Anaother way to write string using new keyword, and its type is object not a String.
+const profileName = new String('thedeepocean')
+console.log(typeof profileName); //object
+
+console.log(profileName);
+console.log(profileName.__proto__);
+
+console.log(gameName.charAt(2)
+/);
+
