ead3ef90cec8d71c94023dd18b5a0ff828929039
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-25 23:46:03 +0530

    nums

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -4,22 +4,24 @@ const name= "Prashant"
 const repoCount = 22
 console.log(name + repoCount + "total");
 
-//string interpolation (backtikk) recommended
+//string interpolation (backtick ) recommended
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
 
 // Anaother way to write string using new keyword, and its type is object not a String.
 const profileName = new String('Prashant') //object
 console.log(typeof profileName); 
-console.log(profileName);  //[String: 'thedeepocean']
-console.log(profileName[0]);  //t
+console.log(profileName);  //[String: 'Prashant']
+console.log(profileName[0]);  //P
 
 
 console.log(profileName.__proto__); //[[Prototype]] : String,,this used to check String's functions/method
 //ye jakar console par dekho sare methods/functions
 
 
+console.log("length", profileName.length);//this is a property of string
+
+
 //this is a function/method of String
-console.log("length", profileName.length);
 console.log(profileName.toUpperCase());
 
 console.log(profileName.charAt(2));  
@@ -32,7 +34,7 @@ console.log(newString);
 /*NOte- we can see that all changes are happening into duplicate data, original valuse is not chaning */
 
 //Unlike substring(), slice() allows negative indexes.
-const anotherString = profileName.slice(-8,4)
+const anotherString = profileName.slice(-8,4)  //o/p- Pras  //count negative index- -8,-7,-6,-5,-4,-3,-2,-1
 console.log(anotherString);
 
 const newStringone = "   Prashant     "
@@ -45,9 +47,9 @@ console.log(url.replace('%20','-')); // https://erprashant.tech/prashant-vishwak
 
 console.log(url.includes('prashant')); //true //we can check also that this word are exist or not in url
 
-const newName = new String('Prashant-vishwakarma')
+const newName = new String('Prashant-vishwakarma-jaunpur-UP-India')
 
-console.log(newName.split('-'));
+console.log(newName.split('-'));  //there is no limit for spliting
 console.log(newName.split('-', 2));//split(separator: value, limit : number) kitna string split karna hai vo define karta hai limit
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
---
+++ 01_Basics/07_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)  //A method to define Number intentionally
+console.log(balance);  //[Number: 100]
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //100.00 (two digit after decimal)
+
+
+const otherNumber = 25.8966 //25.9
+// const otherNumber = 125.8966  //126
+// const otherNumber = 1125.8966   //1.13e+3  means: 1.13 × 10³
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString());  //10,00,000 This automatically uses indian numeric system, we don't need to define (en-IN)
+console.log(hundreds.toLocaleString('en-IN'));  //10,00,000
+console.log(hundreds.toLocaleString('en-US')); //1,000,000
+
