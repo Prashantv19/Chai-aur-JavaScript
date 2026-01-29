bcf1a39a6ae2b991b1c9e546bb6acedbf58e6de7
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-29 22:04:03 +0530

    update

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -1,16 +1,26 @@
 //Comparision Operators
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+console.log(2 > 1); //true
+console.log(2 >= 1); //true
+console.log(2 < 1); //false
+console.log(2 == 1); //false
+console.log(2 != 1); //true
+console.log(" ");
+/*---------------------------------------------------------------------------------------------- */
 
-console.log("2" > 1);// problem starts here due to comparision of different data types
-console.log("02" > 1);
+console.log("2" > 1); // true // problem starts here due to comparision of different data types
+console.log("02" > 1); // true
+console.log(" ");
+/*---------------------------------------------------------------------------------------------- */
+
 //iss tarah ke conversions ko hamesha avoid karna chahiye.
 console.log(null > 0);  // false
 console.log(null == 0); // false
 console.log(null >= 0);  // true
+console.log(" ");
 //the reason is that an equality check == and comparision > < >= <= work differently. comparision converts (3)null >=0 is true and null > 0 is false.
 //the second one is false because it(==) does not convert null to 0
-console.log("2" === 2); //(===) means strictly check, it checks datatype also
+/*---------------------------------------------------------------------------------------------- */
+
+console.log("2" === 2); // false //(===) means strictly check, it checks datatype also
+
+/*---------------------------------------------------------------------------------------------- */
