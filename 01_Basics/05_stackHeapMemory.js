4e84b54d27706f4ebf76df7a465f5f6b6f541c35
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-29 22:59:24 +0530

    revised

--- 01_Basics/05_stackHeapMemory.js
+++ 01_Basics/05_stackHeapMemory.js
@@ -1,11 +1,16 @@
-// Stack (all Primitive data types)- jab koi memory stack me define/store hoti hai toh waha se hame variable ka ek copy milta hai.
-// Heap (all Non-Primitive data types)- aur jab koi memory heap me define/store hoti hai toh waha se referance milta hai original value ka.
+/*---------------------------------------------------------------------------------------------- */
+//STACK
+// Stack (all Primitive data types)- jab koi memory stack me define/store hoti hai toh waha se hame variable ka ek copy milta hai, original value me koi change nhi hota hai.
+
 let myName = "PrashantVishwakarma"
 let yourName = myName
-yourName = "Navneet"
+yourName = "Rudra Vishwakarma"
 
-console.log(myName);
-console.log(yourName); //yha par hum copy me changes karte hai original data par koi effect nhi padta hai.
+console.log(myName);  //Prashant Vishwakarma
+console.log(yourName); //Rudra Vishwakarma //yha par hum copy me changes karte hai original data par koi effect nhi padta hai.
+/*---------------------------------------------------------------------------------------------- */
+//HEAP
+// Heap (all Non-Primitive data types)- aur jab koi memory heap me define/store hoti hai toh waha se original value ka referance milta hai na ki copy ka. and jab hum new variable me koi changes karte hai toh vo original value me bhi hoa hai.
 
 let userOne = {
     email: "name@gmail.com",
@@ -14,7 +19,8 @@ let userOne = {
 
 let userTwo = userOne
 
-userTwo.email= "prashant@gmail.com" //user two ka email change kiya toh user one ka emal bhi change ho jayega, bas itna hi.
+userTwo.email= "prashant@gmail.com" //usertwo ka email change kiya toh user one ka email bhi change ho jayega, bas itna hi.
 
-console.log(userOne.email);
-console.log(userTwo.email);
+console.log(userOne.email);  //prashant@gmail.com
+console.log(userTwo.email); //prashant@gmail.com
+/*---------------------------------------------------------------------------------------------- */
