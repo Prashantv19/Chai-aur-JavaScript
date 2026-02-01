c4fd5b2d372f2277de6fe2c2e6f58d680e740034
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-01 22:48:07 +0530

    1st Feb

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -2,44 +2,50 @@
 // note:- These data types are Call by value, when we call it , we get its copy not original data, aur jo bhi changes hote hai vo copy me hote hai original data me nahi hota hai.
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+//____________________________________________________________________________________________
 
 const score = 100  //Type : Number
 const scoreValue = 100.3 //Type : Number
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false //Boolean
+const outsideTemp = null //Null
 let userEmail;
-// -------------------------------------------------------------
+//____________________________________________________________________________________________
+
 const id = Symbol('123')  // symbol ek unique data type hai, jiska return type bhi symbol hi hota hai.
 const anotherId = Symbol('123')
 //console.log(id === anotherId);  //false
-// -------------------------------------------------------------
+//____________________________________________________________________________________________
+
 const bigNumber = 9812658965895241578n  //last me 'n' lagane se bigInt datatype ban jayega, nhi toh Number hi rahega
 //console.log(typeof bigNumber);
+//____________________________________________________________________________________________
 
-// -------------------------------------------------------------
 //**Javascript is a Dynamically Type Language. because it defines data types by itself. we don't need to define them.
+//____________________________________________________________________________________________
 
-// -------------------------------------------------------------
 // Referance (Non-Primitive)
-//Array, objects, Functions
-// -------------------------------------------------------------
+// Array, objects, Functions
+//____________________________________________________________________________________________
+
 //Array-
 const heros = ["Ben10" , "Avengers", "Ironman"]
-// -------------------------------------------------------------
+//____________________________________________________________________________________________
+
 //Objects-
 let myobj = {
     name: "Prahant",
     age: 23,
 
 }
-// -------------------------------------------------------------
+//____________________________________________________________________________________________
+//Functoin
 const myFunction = function(){
     console.log("This is a function");
     
 }
 console.log(typeof myFunction);  //function
-
+//____________________________________________________________________________________________
 
 console.log(typeof myobj);  //object
 
@@ -54,6 +60,5 @@ console.log(typeof outsideTemp);  //object
 console.log(typeof isLoggedIn);  //boolean
 
 console.log(typeof scoreValue);  //number
-
-
+//____________________________________________________________________________________________
 // https://262.ecma-international.org/5.1/#sec-11.4.3
--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,1 +1,57 @@
 console.log("Let's Start Functon");
+
+function myFunction(){
+    console.log("P")
+    console.log("R")
+    console.log("A")
+    console.log("S")
+    console.log("H")
+    console.log("A")
+    console.log("N")
+    console.log("T")
+}
+myFunction() //AISE FUNCTION KO EXECUTIVE KARTE HAI
+//Parameter and argument?
+// function addTwoNo(num1, num2){
+// console.log(num1+num2);
+// }
+// addTwoNo(5,6) //11
+// addTwoNo(5,"6") //56
+// addTwoNo(5,"a") //5a
+// addTwoNo(5,null) //5
+
+// const result = addTwoNo(4,5) //9 //ye line bhi executive hogi
+// console.log("result: ", result); //result:  undefined; because function return kuchh bhi nahi kar rha hai.
+
+function addTwoNo(num1, num2){
+    let result = num1 + num2
+    return result
+    console.log("Prashant"); //return ke baad kuchh bhi executive nhi hota hai.
+}
+const result = addTwoNo(4,5)
+// console.log("result: ", result);
+/*Note:- Jab hum function return karte hai tab hame console alag se likhana padta hai */
+
+// function loginusermsg(username){
+//     return `${username} just logged in`
+// }
+// console.log(loginusermsg("Prashant"));//Prashant just logged in
+// console.log(loginusermsg("Navneet"));
+// console.log(loginusermsg("Aditya"));
+// console.log(loginusermsg("")); //just logged in
+// console.log(loginusermsg()); //undefined just logged in
+
+//agar parameter undefined hai then
+function loginusermsg(username){
+    if(username === undefined){
+    //if(!username){
+        console.log("Please Enter a Username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginusermsg()); //Please Enter a Username
+
+//atleast prints default value
+// function loginusermsg(username ="Sam"){}
+//jab hum koi argument pass karenge toh default value override kar di jayegi
