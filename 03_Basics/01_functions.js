97c294d472607dfcd3f7eac8dfece9cbf007d0a7
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-01 23:31:08 +0530

    function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,4 +1,4 @@
-console.log("Let's Start Functon");
+// console.log("Let's Start Functon");
 
 function myFunction(){
     console.log("P")
@@ -10,7 +10,7 @@ function myFunction(){
     console.log("N")
     console.log("T")
 }
-myFunction() //AISE FUNCTION KO EXECUTIVE KARTE HAI
+// myFunction() //AISE FUNCTION KO EXECUTIVE KARTE HAI
 //Parameter and argument?
 // function addTwoNo(num1, num2){
 // console.log(num1+num2);
@@ -50,8 +50,43 @@ function loginusermsg(username){
     }
     return `${username} just logged in`
 }
-console.log(loginusermsg()); //Please Enter a Username
+// console.log(loginusermsg()); //Please Enter a Username
 
 //atleast prints default value
 // function loginusermsg(username ="Sam"){}
 //jab hum koi argument pass karenge toh default value override kar di jayegi
+
+
+//------------------------next video------------
+// rest function
+//  function cartPrice(...num1){   //[ 200, 300, 350, 450, 2000 ]
+
+ function cartPrice(val1,val2,...num1){ //200 and 300 , val1,val2 me chale gaye hai  //[ 350, 450, 2000 ]
+    return num1
+ }
+//  console.log(cartPrice(200,300,350,450,2000)); 
+ 
+//create a object
+const user ={
+    username: "Prashant",
+    price: 199
+}
+//create a function to handle object
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user) //Username is Prashant and price is 199
+//aise direct bhi object pass kar sakte hai
+handleObject({          //Username is Rudra and price is 188
+    username: "Rudra",
+    price:188
+})
+
+//Passing array into function
+const newarray =[200,400,600,800]
+function returnFirstValue(getArray){
+    return getArray[1]
+}
+console.log(returnFirstValue(newarray)); //400
+console.log(returnFirstValue([200,400,600,800])); //400
