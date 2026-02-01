b458621191c68157eb360191e95bd8b4c582a184
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-01 21:07:21 +0530

    practice

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,7 @@
-console.log("Prashant")
+let bio = {
+    name : "Prashant Vishwakarma",
+    rollNo: 141,
+    age: 23,
+    course: "B.tech"
+}
+console.log(`My name is ${bio.name}. my Roll No. is ${bio.rollNo}, My Age is ${bio.age}, My Course is ${bio.course}`)
