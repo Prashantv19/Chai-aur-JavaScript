666bf0d9fbcadb7e905f379c9d5c69dc882e1c88
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-02 23:56:56 +0530

    scopeInDetail

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,7 +9,7 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner", a); // ander ki chij ander hi rahegi
+    // console.log("Inner", a); // ander ki chij ander hi rahegi
     
     var c = 30
     c = 30  //aise bhi same problem aayegi
@@ -19,6 +19,21 @@ if (true){
 // console.log(b); // b is not defined
 // console.log(c); //But C execited - (30), this is the problem 
 
- console.log(a); //300 // if ke ander ki value anderr hi rahegi, bahar nhi aayegi
+//  console.log(a); //300 // if ke ander ki value anderr hi rahegi, bahar nhi aayegi
 
  //Note:- console ke ander globle scope alag hota hai and node environment ke ander globle scope alag alag hota hai.
+
+
+
+ //---------------------------new video-------------------------
+ function one(){
+    const username = "Naveen"
+    function two(){
+        const website = "YouTube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+ }
+ one()
