8b666eb73d5f3148bfa2dcb817bb6bc83bf47daa
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-07 12:09:24 +0530

    revised one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,4 +1,5 @@
 // if
+
 // if(3!=2){
 //     console.log("this line of code will run, because condition is true");
 // }
@@ -6,14 +7,16 @@
 // if(3=2){
 //     console.log("this line of code will not run, because condition is false");
 // }
-// const isUserloggedIn = true
-// const temperature = 41
-//--------------------------------------------------------------
 
-// if ( temperature === 40 ){   //O/P-> //temperature is greater than 50
-//     console.log("less than 50");
+// const isUserloggedIn = true   //ye toh hamesha true hi rahega
+
+//--------------------------------------------------------------
+//if-else
+// const temperature = 39
+// if ( temperature === 40 ){   //O/P-> //Not-Equal to 40
+//     console.log("Equal to 40");
 // } else {
-//     console.log("temperature is greater than 50");
+//     console.log("Not-Equal to 40");
 // }
 //--------------------------------------------------------------
 
@@ -27,8 +30,8 @@
 // const score = 200
 
 // if (score > 100) {
-//     let power = "fly"
-//     var power = "fly"  //this is global scope variable
+//     var power = "fly"  //this is global scope variable, it can be accessed
+//     let power = "fly"  // but this is not
 //     console.log(`User power: ${power}`); //User Power: fly 
 // }
 
@@ -36,9 +39,11 @@
 //--------------------------------------------------------------
 
 const balance = 1000
-// implicite scope- isme hum ek sath multiple line likha sakte hai.
+// implicite scope- ye man leta hai ki ye scope ke ander hai. isme hum ek sath multiple line likha sakte hai.
+
 // if (balance > 500) console.log("test") //it is okey but
 // if (balance > 500) console.log("test"),console.log("test2"); //it is not recommended
+
 //--------------------------------------------------------------
 
 //nested if-else if-else if-if
@@ -48,14 +53,14 @@ const balance = 1000
 //     console.log("less than 750");
     
 // } else if (balance < 900) {
-//     console.log("less than 750");
+//     console.log("less than 900");
     
 // } else {
 //     console.log("less than 1200");
 
 // }
 //--------------------------------------------------------------
-// real life senario
+// real life senarios using Logical operators
 const userLoggedIn = true
 const debitCard = true
 const loggedInFromGoogle = false
@@ -65,10 +70,10 @@ const loggedInFromEmail = true
 //AND - Both(all) condition should be true
 //OR - Atleast one condition should be true
 if (userLoggedIn && debitCard && 2==3) {
-    console.log("Allow to buy course");
+    // console.log("Allow to buy course");
 }
 
 if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
+    // console.log("User logged in");
 }
 //--------------------------------------------------------------
