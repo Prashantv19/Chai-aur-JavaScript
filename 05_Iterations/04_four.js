05005a7bd5e717290d1afd06d71cbee908fda008
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-02-08 23:32:31 +0530

    commit

--- 05_Iterations/04_four.js
+++ 05_Iterations/04_four.js
@@ -7,14 +7,26 @@ const myObject = {
 }
 //for-in loop
 //for object
-// for (const key in myObject) {
+
+for (const key in myObject) {
 //    console.log(`${key} for ${myObject[key]}`);
-// }
+}
 //note:hum object ki key use karke uski value find kar sakte hai, but yha . se access hi hoga kyoki ye key nhi hai variable hai so [] ke ander likhenge. 
 
 //for array
 const programming = ['js', 'rb', "java","cpp", "python"]
 for (const key in programming) {
-   console.log(programming[key]);
+//    console.log(programming[key]);
 }
 
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "united states of America")
+map.set('Fr', "France")
+map.set('Ge', "Germani")
+
+for (const key in map) {
+   console.log(key);
+   
+}
+//map ke upar iteration nhi kiya ja sakta hai
---
+++ 05_Iterations/05_foreachLoop.js
@@ -0,0 +1,55 @@
+//for-each loop ek inbuilt function hai Array me
+const coding = ['js', 'rb', "java","cpp", "python"]
+
+//callback function ke name nhi hote hai
+//syntax
+// coding.forEach( function (variable name) {
+//     //what we want to do
+// })
+
+coding.forEach( function (val) {
+    // console.log(val);
+    
+})
+
+//using arrow function
+coding.forEach(  (val) => {
+    // console.log(val);
+    
+})
+
+//another way
+function printMe (item){
+    // console.log(item);
+}
+
+// coding.forEach(printMe);
+
+// this have more parameter except than 'item'
+coding.forEach((item, index, arr)=> {
+    // console.log(item, index, arr);
+})
+
+const mycoding = [
+    {
+        langName: "JavaScript",
+        langFileName: "js"
+    },
+    {
+        langName: "JavaScript",
+        langFileName: "js"
+    },
+    {
+        langName: "JavaScript",
+        langFileName: "js"
+    },
+    {
+        langName: "JavaScript",
+        langFileName: "js"
+    }
+]
+mycoding.forEach((item)=>{
+    console.log(item.langName);
+    console.log(item.langFileName);
+    
+})
